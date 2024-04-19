92a5e3f1f41a5551bac11e501419c27357c3a8ee
kim-h14 <kim.heuacker@gmail.com>
2024-04-19 17:14:21 +0200

    Express path to static files fixed to access bootstrap and assets + video assets added

--- app.js
+++ app.js
@@ -6,18 +6,32 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the root directory
-app.use(express.static(path.join(__dirname, 'public', 'pages')));
+app.use(express.static(path.join(__dirname, '/public', '/pages')));
+
+// Serve static files from the node_modules directory
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
+
+// Serve static files from the assets directory
+app.use('/Assets', express.static(path.join(__dirname, 'assets')));
+
+// Set the correct MIME type for CSS files
+app.use((req, res, next) => {
+  if (req.url.endsWith('.css')) {
+    res.setHeader('Content-Type', 'text/css');
+  }
+  next();
+});
 
 // Define a route for the home page
 app.get('/', (req, res) => {
   // Read the content of home.html
-  fs.readFile(path.join(__dirname, 'public', 'pages', 'home.html'), 'utf8', (err, homeContent) => {
+  fs.readFile(path.join(__dirname, '/public', '/pages', '/home.html'), 'utf8', (err, homeContent) => {
     if (err) {
       return res.status(500).send('Error reading home.html');
     }
 
     // Read the content of index.html
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), 'utf8', (err, indexContent) => {
+    fs.readFile(path.join(__dirname, '/public', '/index.html'), 'utf8', (err, indexContent) => {
       if (err) {
         return res.status(500).send('Error reading index.html');
       }
@@ -30,5 +44,6 @@ app.get('/', (req, res) => {
     });
   });
 });
+
 
 module.exports = app;
