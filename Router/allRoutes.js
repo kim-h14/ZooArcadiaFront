883caeed0f45de575fd40aaf03d676a34cc652dd
kim-h14 <kim.heuacker@gmail.com>
2024-04-22 14:53:49 +0200

    content for html pages + routing

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,9 +3,12 @@ import Route from "./Route.js";
 // Define paths here
 export const allRoutes = [
   new Route("/", "Acceuil", "/pages/home.html"),
-  new Route("/habitat", "Nos habitats", "/pages/habitat.html"),
+  new Route("habitats", "Nos habitats", "/pages/habitats.html"),
   new Route("/services", "Nos services", "/pages/services.html"),
   new Route("/reviews", "Avis", "/pages/reviews.html"),
+  new Route("/animals", "Nos animaux", "/pages/animals.html"),
+  new Route("/contact", "Contactez nous", "/pages/contact.html"),
+  new Route("/login", "Connexion", "/pages/login.html"),
 ];
 
   // The title is the title of the page that will be displayed in the browser tab
