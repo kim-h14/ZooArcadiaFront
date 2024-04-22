16cd45ad0bdbaad0dd3c1a8cb8e9cb436e32466f
kim-h14 <kim.heuacker@gmail.com>
2024-04-22 13:12:40 +0200

    content without style for animals, reviews, services

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,6 +4,8 @@ import Route from "./Route.js";
 export const allRoutes = [
   new Route("/", "Acceuil", "/pages/home.html"),
   new Route("/habitat", "Nos habitats", "/pages/habitat.html"),
+  new Route("/services", "Nos services", "/pages/services.html"),
+  new Route("/reviews", "Avis", "/pages/reviews.html"),
 ];
 
   // The title is the title of the page that will be displayed in the browser tab
