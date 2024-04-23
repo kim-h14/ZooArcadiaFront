7be3c9583e4cdd95efa09f6a4a0a43f2408ba0bd
kim-h14 <kim.heuacker@gmail.com>
2024-04-23 14:30:26 +0200

    Route folder moved to public folder

--- public/Router/allRoutes.js
+++ public/Router/allRoutes.js
@@ -2,13 +2,13 @@ import Route from "./Route.js";
 
 // Define paths here
 export const allRoutes = [
-  new Route("/", "Acceuil", "/public/pages/home.html"),
-  new Route("/habitats", "Nos habitats", "/public/pages/habitats.html"),
+  new Route("/", "Acceuil", "/pages/home.html"),
+  new Route("/habitats", "Nos habitats", "/pages/habitats.html"),
   new Route("/services", "Nos services", "/pages/services.html"),
   new Route("/reviews", "Avis", "/pages/reviews.html"),
   new Route("/animals", "Nos animaux", "/pages/animals.html"),
   new Route("/contact", "Contactez nous", "/pages/contact.html"),
-  new Route("/login", "Connexion", "/pages/login.html", "/js/auth/login.js"),
+  new Route("/login", "Connexion", "/pages/login.html"),
 ];
 
   // The title is the title of the page that will be displayed in the browser tab
