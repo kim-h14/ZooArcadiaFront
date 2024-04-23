538211882a95b67eacfd34c7ca45404549f43349
kim-h14 <kim.heuacker@gmail.com>
2024-04-23 16:49:52 +0200

    Bug fix - define a route to serve index.html for all routes

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const port = process.env.PORT || 3000;
 // Serve static files from the root directory
 app.use(express.static(path.join(__dirname, '/public')));
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, '/public', '/pages')));
+
 // Serve static files from the node_modules directory
 app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 
@@ -25,28 +28,64 @@ app.use((req, res, next) => {
   next();
 });
 
-// Define a route for the home page
-app.get('/', (req, res) => {
-  // Read the content of home.html
-  fs.readFile(path.join(__dirname, '/public', '/pages', '/home.html'), 'utf8', (err, homeContent) => {
+// Define a route to serve index.html for all routes
+app.get('*', (req, res) => {
+  // Read the content of index.html
+  fs.readFile(path.join(__dirname, '/public', '/index.html'), 'utf8', (err, indexContent) => {
     if (err) {
-      return res.status(500).send('Error reading home.html');
+      return res.status(500).send('Error reading index.html');
     }
 
-    // Read the content of index.html
-    fs.readFile(path.join(__dirname, '/public','/index.html'), 'utf8', (err, indexContent) => {
-      if (err) {
-        return res.status(500).send('Error reading index.html');
-      }
-
-      // Merge the content of home.html with index.html
-      const mergedContent = indexContent.replace('<!-- INSERT_HOME_CONTENT -->', homeContent);
-
-      // Send the merged content in the response
-      res.send(mergedContent);
-    });
+    // Send index.html as response for all routes
+    res.send(indexContent);
   });
 });
+
+// // Define a route for the home page
+// app.get('/', (req, res) => {
+//   // Read the content of home.html
+//   fs.readFile(path.join(__dirname, '/public', '/pages', '/home.html'), 'utf8', (err, homeContent) => {
+//     if (err) {
+//       return res.status(500).send('Error reading home.html');
+//     }
+
+//     // Read the content of index.html
+//     fs.readFile(path.join(__dirname, '/public','/index.html'), 'utf8', (err, indexContent) => {
+//       if (err) {
+//         return res.status(500).send('Error reading index.html');
+//       }
+
+//       // Merge the content of home.html with index.html
+//       const mergedContent = indexContent.replace('<!-- INSERT_HOME_CONTENT -->', homeContent);
+
+//       // Send the merged content in the response
+//       res.send(mergedContent);
+//     });
+//   });
+// });
+
+// Define a route for the animals page
+// app.get('/animals', (req, res) => {
+//   // Read the content of animals.html
+//   fs.readFile(path.join(__dirname, '/public', '/pages', '/animals.html'), 'utf8', (err, animalsContent) => {
+//     if (err) {
+//       return res.status(500).send('Error reading animals.html');
+//     }
+
+//     // Read the content of index.html
+//     fs.readFile(path.join(__dirname, '/public','/index.html'), 'utf8', (err, indexContent) => {
+//       if (err) {
+//         return res.status(500).send('Error reading index.html');
+//       }
+
+//       // Merge the content of animals.html with index.html
+//       const mergedContent = indexContent.replace('<!-- INSERT_HOME_CONTENT -->', animalsContent);
+
+//       // Send the merged content in the response
+//       res.send(mergedContent);
+//     });
+//   });
+// });
 
 
 
