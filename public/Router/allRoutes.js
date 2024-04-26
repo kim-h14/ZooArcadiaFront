812195dd47b0a4b76391523286864cab3cdc8a71
kim-h14 <kim.heuacker@gmail.com>
2024-04-26 11:28:20 +0200

    routing to habitat from home.html buttons

--- public/Router/allRoutes.js
+++ public/Router/allRoutes.js
@@ -3,7 +3,7 @@ import Route from "./Route.js";
 // Define paths here
 export const allRoutes = [
   new Route("/", "Acceuil", "/pages/home.html", "/js/animations/home.js"),
-  new Route("/habitats", "Nos habitats", "/pages/habitats.html"),
+  new Route("/habitats", "Nos habitats", "/pages/habitats.html", "/js/animations/habitats.js"),
   new Route("/services", "Nos services", "/pages/services.html"),
   new Route("/reviews", "Avis", "/pages/reviews.html", "/js/auth/reviews.js"),
   new Route("/animals", "Nos animaux", "/pages/animals.html", "js/animations/animals.js"),
---
+++ public/js/animations/habitats.js
@@ -0,0 +1,15 @@
+// // ======= Scrolling option to right section =======
+// // to be used when navigating from home page <a> tags inside the habitat-btn
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   // Check if there is a fragment identifier in the URL
+//   if (window.location.hash) {
+//       // Get the ID of the target section from the URL
+//       var targetId = window.location.hash.substring(1); // Remove the '#' character
+//       // Scroll to the target element
+//       var targetElement = document.getElementById(targetId);
+//       // if (targetElement) {
+//       //     targetElement.scrollIntoView({ behavior: 'smooth' });
+//       // }
+//   }
+// });
