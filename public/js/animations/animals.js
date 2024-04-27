b8958ae839cdec5130bc729a0667f95644c1508d
kim-h14 <kim.heuacker@gmail.com>
2024-04-27 11:27:05 +0200

    animations for animals and home page created Css + JS

--- public/js/animations/animals.js
+++ public/js/animations/animals.js
@@ -1,3 +1,46 @@
+// ======= ANIMATIONS =======
+
+// ++++++++ <3 button animation and counter ++++++++
+// Check if the page is fully loaded
+window.onload = function() {
+  console.log("Page fully loaded");
+
+  const heartIcon = document.querySelector(".heart");
+  console.log(heartIcon); // Check if the heart icon is correctly selected
+
+  const likeCounter = document.querySelector(".like-counter");
+  console.log(likeCounter); // Check if the like counter is correctly selected
+
+  let likeCount = 0;
+
+  heartIcon.addEventListener("click", function(event) {
+      console.log("Heart icon clicked"); // Check if the heart icon click event is triggered
+
+      event.preventDefault(); // Prevent default behavior of the anchor tag
+      likeCount++;
+      likeCounter.textContent = likeCount;
+      likeCounter.style.display = "inline";
+
+      // Create flying heart emoji
+      const flyingHeart = document.createElement("span");
+      flyingHeart.textContent = "❤️";
+      flyingHeart.classList.add("flying-heart");
+      document.body.appendChild(flyingHeart);
+
+      // Remove flying heart emoji after animation ends
+      flyingHeart.addEventListener("animationend", function() {
+          console.log("Flying heart animation ended"); // Check if the flying heart animation ends
+          flyingHeart.remove();
+      });
+  });
+};
+
+
+
+
+
+
+// ++++++++ "En savoir plus" button animation ++++++++
 $(document).ready(function() {
   $('.card__animal__btn').click(function() {
       // Toggle the display of the card details
--- public/js/animations/home.js
+++ public/js/animations/home.js
@@ -1,28 +1,32 @@
-// $(document).ready(function() {
-//   let scrollPosition = 0;
-//   const cardWidth = $('.carousel-item').outerWidth();
-//   const carouselWidth = $('.carousel-inner').outerWidth();
-//   const numCards = $('.carousel-item').length;
+// ======= Video animation =======
+// Get reference to the video element
+const video = document.getElementById('video-bg');
 
-//   $('.carousel-control-next').on("click", function(){
-//       if (scrollPosition < (carouselWidth - cardWidth)){
-//           scrollPosition += cardWidth;
-//       } else {
-//           scrollPosition = 0; // Reset scroll position to start
-//       }
-//       $(".carousel-inner").animate({scrollLeft: scrollPosition}, 600);
-//   });
+const videoSources = [
+  "/assets/videos/elephantVideo.mp4",
+  "/assets/videos/giraffeVideo.mp4",
+  "/assets/videos/lemurVideo.mp4",
+  "/assets/videos/ostrichVideo.mp4",
+  "/assets/videos/otterVideo.mp4",
+  "/assets/videos/tigerVideo.mp4",
+];
 
-//   $('.carousel-control-prev').on("click", function(){
-//       if (scrollPosition > 0){
-//           scrollPosition -= cardWidth;
-//       } else {
-//           scrollPosition = (numCards - 1) * cardWidth; // Scroll to the last card
-//       }
-//       $(".carousel-inner").animate({scrollLeft: scrollPosition}, 600);
-//   });
-// });
+// Index of current video source
+var currentSourceIndex = 0;
 
+  video.addEventListener('ended', function() {
+    console.log('Video ended');
+    currentSourceIndex++;
+    if (currentSourceIndex >= videoSources.length) {
+      currentSourceIndex = 0;
+    }
+    console.log('Loading video: ' + videoSources[currentSourceIndex]);
+    video.src = videoSources[currentSourceIndex];
+    video.load();
+  });
+
+
+// ======= Carousel animation =======
 $(document).ready(function() {
   let scrollPosition = 0;
   const cardWidth = $('.carousel-item').outerWidth();
