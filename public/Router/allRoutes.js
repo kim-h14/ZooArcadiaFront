078503a40532fd25d3092b01aff0c500f1b5af39
kim-h14 <kim.heuacker@gmail.com>
2024-04-29 12:07:35 +0200

    routing authentification updated

--- public/Router/allRoutes.js
+++ public/Router/allRoutes.js
@@ -3,12 +3,12 @@ import Route from "./Route.js";
 // Define paths here
 export const allRoutes = [
   new Route("/", "Acceuil", "/pages/home.html", [], "/js/animations/home.js"),
-  new Route("/habitats", "Nos habitats", "/pages/habitats.html", [], "/js/animations/habitats.js"),
-  new Route("/services", "Nos services", "/pages/services.html", []),
-  new Route("/reviews", "Avis", "/pages/reviews.html", [], "/js/auth/reviews.js"),
-  new Route("/animals", "Nos animaux", "/pages/animals.html", [], "js/animations/animals.js"),
+  new Route("/habitats", "Nos habitats", "/pages/habitats.html", [["disconnected"], ["connected", "administrator"], ["connected", "vet"]], "/js/animations/habitats.js"),
+  new Route("/services", "Nos services", "/pages/services.html", [["disconnected"], ["connected", "administrator"], ["connected", "employee"]]),
+  new Route("/reviews", "Avis", "/pages/reviews.html", [["disconnected"], ["connected", ["employee"]]], "/js/auth/reviews.js"),
+  new Route("/animals", "Nos animaux", "/pages/animals.html", [["disconnected"], ["connected", "administrator"], ["connected", "employee"], ["connected", "vet"]], "js/animations/animals.js"),
   new Route("/contact", "Contactez nous", "/pages/contact.html", [], "/js/auth/contact.js"),
-  new Route("/login", "Connexion", "/pages/login.html", ["disconnected"], "/js/auth/login.js"),
+  new Route("/login", "Connexion", "/pages/login.html", [], "/js/auth/login.js"),
   new Route("/ethics", "Notre éthique", "/pages/ethics.html", []),
 ];
 
