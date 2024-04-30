9a7bc133887fae007d7b5329908126862e2b799b
kim-h14 <kim.heuacker@gmail.com>
2024-04-30 08:50:39 +0200

    login form fixed

--- public/Router/allRoutes.js
+++ public/Router/allRoutes.js
@@ -6,7 +6,7 @@ export const allRoutes = [
   new Route("/habitats", "Nos habitats", "/pages/habitats.html", [["disconnected"], ["connected", "administrator"], ["connected", "vet"]], "/js/animations/habitats.js"),
   new Route("/services", "Nos services", "/pages/services.html", [["disconnected"], ["connected", "administrator"], ["connected", "employee"]]),
   new Route("/reviews", "Avis", "/pages/reviews.html", [["disconnected"], ["connected", ["employee"]]], "/js/auth/reviews.js"),
-  new Route("/animals", "Nos animaux", "/pages/animals.html", [["disconnected"], ["connected", "administrator"], ["connected", "employee"], ["connected", "vet"]], "js/animations/animals.js"),
+  new Route("/animals", "Nos animaux", "/pages/animals.html", [], "js/animations/animals.js"),
   new Route("/contact", "Contactez nous", "/pages/contact.html", [], "/js/auth/contact.js"),
   new Route("/login", "Connexion", "/pages/login.html", [], "/js/auth/login.js"),
   new Route("/ethics", "Notre éthique", "/pages/ethics.html", []),
--- public/js/auth/login.js
+++ public/js/auth/login.js
@@ -131,3 +131,11 @@ function checkCredentials() {
   }
 }
 
+// ============== body parser ==============
+
+import bodyParser from "body-parser";
+import app from "../../../app";
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
