10e1aeb9995c8f829a0bab423797200b87f8d0d2
kim-h14 <kim.heuacker@gmail.com>
2024-07-09 10:27:27 +0200

    updating routing after authentification controller created

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Packages required for the application 
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
@@ -19,7 +20,6 @@ dotenv.config({
 
 const app = express();
 const port = process.env.PORT || 3000;
-const saltRounds = 10;
 
 // Connect to MongoDB using Mongoose
 mongoose.connect('mongodb://localhost:27017/arcardia-consultation')
@@ -118,71 +118,9 @@ app.get('/check-db-connection', async (req, res) => {
   }
 });
 
-// Cookie setting by role
-function setCookie(res, name, value, days) {
-  const options = {
-    maxAge: days * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-  };
-  res.cookie(name, value, options);
-}
-const roleCookieName = 'userRole';
-
 // Define route handler for POST requests to /login
-app.post('/login', async (req, res) => {
-  const { Email, Password } = req.body;
-
-  try {
-    // Check if the provided credentials match the admin credentials
-    if (Email === process.env.ADMIN_EMAIL && Password === process.env.ADMIN_PASSWORD) {
-      // Generate JWT token for admin
-      const token = jwt.sign({ email: Email, role: 'admin' }, 'your_secret_key');
-
-      // Set role cookie for admin
-      setCookie(res, roleCookieName, 'admin', 7);
-
-      // Respond with the token and role
-      return res.status(200).json({ token, role: 'admin' });
-    }
-
-     // Query the database to find a matching user
-     const query = 'SELECT user_id, role, password FROM account WHERE email = $1';
-     const result = await pool.query(query, [Email]);
- 
-     // If a matching user is found
-     if (result.rows.length > 0) {
-       const user = result.rows[0];
-       const hashedPassword = user.password;
- 
-       // Compare the provided password with the hashed password in the database
-       const passwordMatch = await bcrypt.compare(Password, hashedPassword);
- 
-       if (passwordMatch) {
-         // Generate JWT token for user
-         const token = jwt.sign({ user_id: user.user_id, email: Email }, 'your_secret_key');
- 
-         // Set role cookie for user
-         let role = user.role;
-         if (role === 'Employé') role = 'Employé';
-         if (role === 'Vétérinaire') role = 'Vétérinaire';
-         setCookie(res, roleCookieName, role, 7);
- 
-         // Respond with the token and role
-         return res.status(200).json({ token, role });
-       } else {
-         // Password doesn't match, invalid credentials
-         return res.status(401).send('Invalid email or password');
-       }
-     } else {
-       // No matching user found, invalid credentials
-       return res.status(401).send('Invalid email or password');
-     }
-   } catch (error) {
-     console.error('Error authenticating user:', error);
-     return res.status(500).send('Internal Server Error');
-   }
-});
+const authController = require('./controllers/authController');
+app.post('/login', authController.login);
 
 
 // Handle POST requests to create a new staff member for admin Dashboard
